0021e6ab0b8afa0303ec784a695eef53dab373b2
Joel Greyhat <98376482+joelgreyhat1@users.noreply.github.com>
2023-04-16 21:47:21 +0100

    Add files via upload

---
+++ links (1).js
@@ -0,0 +1,53 @@
+export const links = [
+    {
+      name: "Twitter",
+      link: "https://twitter.com/Joel_Greyhat?t=nbfBzg_BIcIgPbNDV2x8ag&s=09",
+      image: "twitter.png",
+    },
+    {
+      name: "Linkedin",
+      link: "https://www.linkedin.com/in/joel-greyhat-4946b4241",
+      image: "linkedin.png",
+    },
+    {
+      name: "Instagram",
+      link: "https://www.instagram.com/joelgreyhat/",
+      image: "instagram.png",
+    },
+    {
+      name: "E-mail",
+      link: "mailto: joelgreyhat@gmail.com",
+      image: "gmail.png",
+    },
+    {
+      name: "Github",
+      link: "https://github.com/joelgreyhat1",
+      image: "github.png",
+    },
+    {
+      name: "WhatsApp",
+      link: "https://wa.me/+2348188274116",
+    },
+    {
+      name: "Facebook",
+      link: "https://www.facebook.com/joelgreyhat",
+    },
+    {
+      name: "Dev.to",
+      link: "https://dev.to/joelgreyhat1",
+    },
+    {
+      name: "Telegram",
+      link: "https://www.t.me/joel_greyhat",
+
+    },
+     {
+      name: "Telegram Channel",
+      link: "https://t.me/jsdc3",
+    },
+     {
+      name: "Facebook Page",
+      link: "https://www.facebook.com/joelgreyhatfbpage?mibextid=ZbWKwL",
+    }
+    
+  ];
